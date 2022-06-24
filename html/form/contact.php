154c58e33975ad5a4048286a71edcb9537e5a454
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-06-23 21:07:24 -0400

    create projects, add tech logos

--- html/form/contact.php
+++ html/form/contact.php
@@ -1,30 +1,24 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <sender@yourdomain.com>';
+$from = "Jacob's Portfolio Site<contact@jacobbenowitz.com>";
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <reciever@example.com>';
+$sendTo = 'Jacob Benowitz <jacob@jacobbenowitz.com>';
 
 // subject of the email
-$subject = 'New message from contact form';
+$subject = "Hello! Let's connect.";
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
 $fields = array('InputName' => 'Name', 'InputEmail' => 'Email', 'InputSubject' => 'Subject', 'InputMessage' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
-$okMessage = 'Your message successfully submitted. Thank you, I will get back to you soon!';
+$okMessage = 'Your message successfully submitted. Thank you, I look forward to connecting with you!';
 
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-/*
- *  LET'S DO THE SENDING
- */
 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
