a80b6027bc025809b16260bcb40635c406825f8c
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-06-24 10:55:29 -0400

    refactor submit func for contact form

--- js/contact.js
+++ js/contact.js
@@ -1,31 +1,60 @@
 $(function () {
-
-    "use strict";
-
     // init the validator
     $('#contact-form').validator();
-
 
     // when the form is submitted
     $('#contact-form').on('submit', function (e) {
 
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
-
-            var messageAlert = 'alert-' + 'success';
-            var messageText = "Your message successfully submitted. Thank you, I look forward to connecting with you!";
-
-            // let's compose Bootstrap alert box HTML
-            var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-            // If we have messageAlert and messageText
-            if (messageAlert && messageText) {
-                // inject the alert to .messages div in our form
-                $('#contact-form').find('.messages').html(alertBox);
-                // empty the form
-                $('#contact-form')[0].reset();
-            }
+            let contactForm = document.getElementById("contact-form");
+            let formData = new FormData(contactForm);
+            fetch("/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams(formData).toString(),
+            })
+            .then(() => console.log("Form successfully submitted"))
+            .catch((error) => alert(error));
         };
+        // $('#contact-form')[0].reset();
         
     });
 });
+
+
+// PREV SUBMISSION FUNC
+
+// $(function () {
+
+//     "use strict";
+
+//     // init the validator
+//     $('#contact-form').validator();
+
+
+//     // when the form is submitted
+//     $('#contact-form').on('submit', function (e) {
+
+//         // if the validator does not prevent form submit
+//         if (!e.isDefaultPrevented()) {
+
+//             var messageAlert = 'alert-' + 'success';
+//             var messageText = "Your message successfully submitted. Thank you, I look forward to connecting with you!";
+
+//             // let's compose Bootstrap alert box HTML
+//             var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    
+//             // If we have messageAlert and messageText
+//             if (messageAlert && messageText) {
+//                 // inject the alert to .messages div in our form
+//                 $('#contact-form').find('.messages').html(alertBox);
+//                 // empty the form
+//                 $('#contact-form')[0].reset();
+//             }
+//         };
+        
+//     });
+// });
